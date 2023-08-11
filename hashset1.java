d1406bb3df5471a5785c6d374500eac95d339482
87678Rutuja <132226247+87678Rutuja@users.noreply.github.com>
2023-08-11 15:26:45 +0530

    Add files via upload

---
+++ hashset1.java
@@ -0,0 +1,13 @@
+import java.util.*;
+class HashsetDemo{
+        public static void main(String[] ar){
+                HashSet hs = new HashSet();
+
+                hs.add("sara");
+                 hs.add("isha");
+                  hs.add("sara");
+                   hs.add("isha");
+
+                   System.out.println(hs);
+        }
+}
